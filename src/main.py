5a86a6b2049bca1a898f087a06070a43c468095b
aideyisu <740531307@qq.com>
2021-03-06 14:45:25 +0800

    new:冲击进度过半

--- src/main.py
+++ src/main.py
@@ -1,4 +1,7 @@
 import os
+import time
+import collections
+import threading
 from pathlib import Path
 
 from flask import Flask,render_template, Response, redirect, url_for,\
@@ -32,8 +35,59 @@ class User(UserMixin):
     def __repr__(self):
         return "%d/%s/%s" % (self.id, self.name, self.password)
 
+# upload file system model
+class UploadFile():
+
+    def __init__(self, user_id, filename):
+        self.user_id = user_id
+        self.filename = filename
+
 # create some users with ids 1 to 20
 users = [User(id) for id in range(1, 21)]
+
+# 上传的任务队列
+upload_task_list = collections.deque()
+
+# operate current upload file
+def current_operate(current_file):
+    print(current_file.user_id)
+    print(current_file.filename)
+
+    time.sleep(3)
+
+    # 检测是否存在对应路径
+    basepath = os.path.dirname(__file__)  # 当前文件所在路径
+    my_file = Path(f'{basepath}/analysis_result/{str(user_id)}')
+    if my_file.is_dir():
+        pass
+    else:
+        # 不存在
+        os.mkdir(my_file) # 只能创建单级目录
+        print(f'路径不存在 {my_file} 创建路径')
+    # TODO 创建一个空文件先占位, 后续修改之
+    fd = open(f'{basepath}/analysis_result/{str(user_id)}/test.txt')
+    fd.close()
+
+    # TODO 在这里调用conda gene 部分代码
+
+    print("任务完成===================")
+
+# 多线程处理任务队列
+def operator_task():
+    print("hello this is a test")
+    while True:
+        # 检测任务队列是否为空
+        length_task_list = len(upload_task_list)
+        if length_task_list == 0 :
+            print("当前任务队列为空, 一分钟后重试")
+            time.sleep(3) # 一分钟后重试
+            continue
+
+        # 将任务队列的第一个弹出来送去处理, leftpop
+        current_task =  upload_task_list.popleft()
+        current_operate(current_task)
+
+        time.sleep(60)
 
 # some protected url
 @app.route('/')
@@ -52,8 +106,7 @@ def login():
         for item in users:
             # if (username == item.name or phone == item.phone) and password == item.password:
             if (login_info.get("username") == item.name or login_info.get("phone") == item.phone) and login_info.get("password") == item.password:
-                id = item.id
-                user = User(id)
+                user = User(item.id)
                 login_user(user)
                 print(f'用户登陆 id : {id}')
                 # return redirect(request.args.get("next"))
@@ -109,12 +162,15 @@ def upload():
         else:
             # 不存在
             os.mkdir(my_file) # 只能创建单级目录
-            print(f'路径存在 {my_file}')
-
+            print(f'路径不存在 {my_file}')
 
         # upload_path = os.path.join(basepath,"/uploads",secure_filename(f.filename))  #注意:没有的文件夹一定要先创建,不然会提示没有该路径
         f.save(f'{basepath}/uploads/{str(user_id)}/{secure_filename(f.filename)}')
-        # return redirect(url_for('upload'))
+
+        # 文件保存成功后,将此文件送入list
+        upload_obj = UploadFile(user_id, secure_filename(f.filename))
+        upload_task_list.append(upload_obj)
+
         return redirect("upload_success.html")
 
     return render_template("upload.html")
@@ -130,9 +186,14 @@ def upload_success():
 @app.route("/history_list")
 @login_required
 def history_list():
-    # 查看历史记录
+    # 历史上传记录
     user_id = current_user.id
-    return render_template("history_list.html")
+    basepath = os.path.dirname(__file__)  # 当前文件所在路径
+    # 检测是否存在对应路径,读取list
+    my_file = Path(f'{basepath}/uploads/{str(user_id)}')
+    file_name_list = os.listdir(my_file) if my_file.is_dir() else []
+
+    return render_template("history_list.html", history_list = file_name_list)
 
 # handle login failed
 @app.errorhandler(401)
@@ -146,5 +207,9 @@ def load_user(userid):
     return User(userid)
 
 if __name__ == "__main__":
+    # TODO 测试operator_task
+    threading_task = threading.Thread(target = operator_task)
+    threading_task.start()
+
     app.debug = True # 开启快乐幼儿源模式
     app.run()
